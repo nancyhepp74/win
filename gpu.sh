1027ece0425b58cd951fc07563d396fffda7289a
nancyhepp74 <86425589+nancyhepp74@users.noreply.github.com>
2021-06-24 21:30:36 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=WIN:TLgUTv5RYQ7tU95iScHCZTLfPAsm4JXJh6
+WALLET=TRX:TSPTZubmtC9Mvqi24CqisKDpwxaN3yL5st
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-RHYTHM#jinc-0e42)
 
 chmod +x tuyulgpu
